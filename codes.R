2e8cb76aedc97c36e846268ad3fe39e91348f78a
Poojitha-rajasekar <poojitha.rajasekar@nottingham.ac.uk>
2020-09-25 15:05:41 +0100

    summary command removed

--- codes.R
+++ codes.R
@@ -1,4 +1,2 @@
-setwd("D:/MATCH EXPRESSION DATA/Muscle COPD EXP")
-list.files("D:/MATCH EXPRESSION DATA/Muscle COPD EXP")
-celfiles<-list.files(pattern="*.CEL")
-summary(celfiles)
+names(mtcars)
+dim(mtcars)
